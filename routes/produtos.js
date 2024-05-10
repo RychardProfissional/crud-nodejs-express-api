56e7db6928b9fdb7b819f22b48ebf4188c401d81
RychardProficional <rychardproficional@gmail.com>
2024-05-10 10:49:28 -0400

    atualizando rota de produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -10,13 +10,67 @@ const pool = mysql.createPool({
 })
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        Produtos: [
-            {
-                id: 1,
-                description: '',
+    pool.getConnection((err, conn) => {
+        if (err) {
+            res.status(417).json({
+                error: { message: 'Erro com ao tentar se conectar com o banco de dados' }
+            })
+            return
+        }
+
+        conn.query("SELECT id, descricao, preco FROM `produtos`", (qErr, result) => {
+            if (qErr) {
+                res.status(500).json({
+                    error: {message: qErr.sqlMessage}
+                })
             }
-        ]
+            else if (!result.length) {
+                res.status(404).json({
+                    error: {message: "Não existe produtos no banco de dados"}
+                })
+            }
+            else {
+                res.status(200).json({Produtos: result})
+            }
+        })
+
+        conn.release()
+    })
+})
+
+router.get('/:id_produto', (req, res, next) => {
+    const idProduto = req.params.id_produto
+    if (!idProduto) {
+        res.status(400).json({
+            error: { message: 'Erro nos dados passados para a API'}
+        })
+        return
+    }
+    
+    pool.getConnection((err, conn) => {
+        if (err) {
+            res.status(417).json({
+                error: { message: 'Erro com ao tentar se conectar com o banco de dados' }
+            })
+            return
+        }
+        
+        conn.query("SELECT descricao, preco FROM `produtos` WHERE `id` = ? LIMIT 1", [idProduto], (qErr, result) => {
+            if (qErr) {
+                res.status(500).json({
+                    error: {message: qErr.sqlMessage}
+                })
+            } 
+            else if (!result.length) {
+                res.status(404).json({
+                    error: {message: "Produto não encontrado"}
+                })
+            }
+            else {
+                res.status(200).json({...result[0]})
+            }
+        })
+        conn.release()
     })
 })
 
@@ -38,32 +92,105 @@ router.post('/', (req, res, next) => {
             return
         }
         
-        conn.query("INSERT INTO `produtos` (descricao, preco) VALUES (?, ?)", [descricao, preco], (qErr, results) => {
+        conn.query("INSERT INTO `produtos` (descricao, preco) VALUES (?, ?)", [descricao, preco], (qErr, result) => {
             if (qErr) {
                 res.status(500).json({
                     error: {message: qErr.sqlMessage}
                 })
-                return
             }
-
-            res.status(200).json({
-                menssage: 'produto cadastrado'
-            })
+            else {
+                res.status(200).json({
+                    message: 'produto cadastrado'
+                })
+            }
         })
 
         conn.release()
     })
 })
 
-router.patch('/', (req, res, next) => {
-    res.status(200).json({
-        menssage: 'produto modificado'
+router.put('/:id_produto', (req, res, next) => {
+    const idProduto = req.params.id_produto
+    const {descricao, preco} = req.body
+
+    if (!idProduto, !descricao, !preco) {
+        res.status(400).json({
+            error: { message: 'Erro nos dados passados para a API'}
+        })
+        return
+    }
+
+    pool.getConnection((err, conn) => {
+        if (err) {
+            res.status(417).json({
+                error: { message: 'Erro com ao tentar se conectar com o banco de dados' }
+            })
+            return
+        }
+        
+        conn.query(
+            "UPDATE `produtos` SET `descricao` = ?, `preco` = ? WHERE `id` = ?", 
+            [descricao, preco, idProduto], 
+            (qErr, result) => {
+                if (qErr) {
+                    res.status(404).json({
+                        error: {message: qErr.sqlMessage}
+                    })
+                }
+                else if (!result.changedRows) {
+                    res.status(404).json({
+                        error: {message: "Produto não encontrado"}
+                    })
+                }
+                else {
+                    res.status(200).json({
+                        message: "Produto atualizado com sucesso"
+                    })
+                }
+            }
+        )
+
+        conn.release()
     })
 })
 
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        menssage: 'produto deletado'
+router.delete('/:id_produto', (req, res, next) => {
+    const idProduto = req.params.id_produto
+
+    if (!idProduto) {
+        res.status(400).json({
+            error: { message: 'Erro nos dados passados para a API'}
+        })
+        return
+    }
+
+    pool.getConnection((err, conn) => {
+        if (err) {
+            res.status(417).json({
+                error: { message: 'Erro com ao tentar se conectar com o banco de dados' }
+            })
+            return
+        }
+        
+        conn.query("DELETE FROM `produtos` WHERE `id` = ?", [idProduto], (qErr, result) => {
+            if (qErr) {
+                res.status(404).json({
+                    error: {message: qErr.sqlMessage}
+                })
+            }
+            else if (!result.changedRows) {
+                res.status(404).json({
+                    error: {message: "Produto não encontrado"}
+                })
+            }
+            else {
+                res.status(410).json({
+                    message: 'Produto deletado com sucesso'
+                })
+            }
+        })
+
+        conn.release()
     })
 })
 
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const produtos = require('./routes/produtos')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(bodyParser.json({ "Content-Type": 'application/json' }))
 
 app.use('/produtos', produtos)
